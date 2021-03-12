216d1336896cb4b9270f29019b0dadb4f059ec6d
Timothy-Barry <timothybarry@cmu.edu>
2021-03-12 15:30:05 -0500

    update at-scale vignette

--- inst/at_scale_scripts/param_file.R
+++ inst/at_scale_scripts/param_file.R
@@ -2,15 +2,14 @@ library(ondisc)
 param_funct <- function(param) {
         switch(param,
                # modify the parameters below
-               storage_dir = "~/Desktop/",
-               n_processors = 40,
-               pod_sizes = c(gene = 3, gRNA = 2, pair = 5),
+               storage_dir = "~/my_sceptre_dir/",
                expression_matrix = ondisc_matrix(system.file("extdata", "expressions.h5", package = "sceptre")),
                perturbation_matrix = ondisc_matrix(system.file("extdata", "perturbations.h5", package = "sceptre")),
                covariate_matrix = readRDS(system.file("extdata", "covariate_matrix.rds", package = "sceptre")),
                gene_gRNA_pairs = readRDS(system.file("extdata", "gene_gRNA_pairs.rds", package = "sceptre")),
-               regularization_amount = 1,
                side = "left",
+               pod_sizes = c(gene = 3, gRNA = 2, pair = 5),
+               regularization_amount = 1,
                seed = 4,
-               B = 500
-               )}
+               B = 500)
+}
