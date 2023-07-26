9b679cf7231a892c115f8cc8c80db9c6c0b82a75
Tim <tbarry2@andrew.cmu.edu>
2023-07-26 10:18:04 -0400

    add checks for the extra covariates

--- R/preprocessing_functions_v2.R
+++ R/preprocessing_functions_v2.R
@@ -5,13 +5,13 @@ check_create_sceptre_object_inputs <- function(response_matrix, grna_matrix, grn
     stop("The data frame `grna_group_data_frame` must have columns `grna_id` and `grna_group`. The `grna_group` column should specify the group to which each `grna_id` belongs.")
   }
 
-  # 3. verify that the row names are unique for both response and grna modalities
+  # 2. verify that the row names are unique for both response and grna modalities
   response_ids <- rownames(response_matrix)
   grna_ids <- rownames(grna_matrix)
   if (length(response_ids) != length(unique(response_ids))) stop("The rownames of the `response_matrix` must be unique.")
   if (length(grna_ids) != length(unique(grna_ids))) stop("The rownames of the `grna_matrix` must be unique.")
 
-  # 4. ensure that the ampersand symbol (&) is absent from the grna ids; ensure that no gRNA is named "non-targeting"
+  # 5. ensure that the ampersand symbol (&) is absent from the grna ids; ensure that no gRNA is named "non-targeting"
   problematic_grna_ids <- grep(pattern = "&", x = grna_ids)
   if (length(problematic_grna_ids) >= 1) {
     stop(paste0("The ampersand character (&) cannot be present in the gRNA IDs. The following gRNA IDs contain an ampersand: ", paste0(grna_ids[problematic_grna_ids], collapse = ", ")))
@@ -20,12 +20,12 @@ check_create_sceptre_object_inputs <- function(response_matrix, grna_matrix, grn
     stop("No individual gRNA can have the ID `non-targeting`. The string `non-targeting` is reserved for the `grna_group` column of the `grna_group_data_frame`.")
   }
 
-  # 5. check that the ids in the grna group data frame are a subset of the ids in the grna matrix
+  # 6. check that the ids in the grna group data frame are a subset of the ids in the grna matrix
   if (!all(grna_group_data_frame$grna_id %in% rownames(grna_matrix))) {
     stop("The column `grna_id` of the `response_grna_group_pairs` data frame must be a subset of the row names of the grna expression matrix.")
   }
 
-  # 9. check type of input matrices
+  # 7. check type of input matrices
   check_matrix_class <- function(input_matrix, input_matrix_name, allowed_matrix_classes) {
     ok_class <- sapply(X = allowed_matrix_classes, function(mat_class) methods::is(input_matrix, mat_class)) |> any()
     if (!ok_class) {
@@ -35,7 +35,7 @@ check_create_sceptre_object_inputs <- function(response_matrix, grna_matrix, grn
   check_matrix_class(response_matrix, "response_matrix", c("matrix", "dgTMatrix", "dgCMatrix", "dgRMatrix"))
   check_matrix_class(grna_matrix, "grna_matrix", c("matrix", "dgTMatrix", "dgCMatrix", "dgRMatrix", "lgTMatrix", "lgCMatrix", "lgRMatrix"))
 
-  # 10. check for agreement in number of cells
+  # 8. check for agreement in number of cells
   check_ncells <- ncol(response_matrix) == ncol(grna_matrix)
   if (!is.null(extra_covariates)) {
     check_ncells <- check_ncells && (ncol(response_matrix) == nrow(extra_covariates))
@@ -44,14 +44,23 @@ check_create_sceptre_object_inputs <- function(response_matrix, grna_matrix, grn
     stop("The number of cells in the `response_matrix`, `grna_matrix`, and `extra_covariates` (if supplied) must coincide.")
   }
 
-  # 11. check that column names of extra_covariates are not already taken
+  # 9. check that column names of extra_covariates are not already taken
   reserved_covariate_names <- c("response_n_nonzero", "response_n_umis", "response_p_mito", "grna_n_nonzero", "grna_n_umis")
   extra_covariate_names <- colnames(extra_covariates)
   if (any(extra_covariate_names %in% reserved_covariate_names)) {
     stop("The covariate names `response_n_nonzero`, `response_n_umis`, `response_p_mito`, `grna_n_nonzero`, and `grna_n_umis` are reserved. Change the column names of the `extra_covariates` data frame.")
   }
 
-  # 12. verify that moi is specified
+  # 10. verify that the types of the extra covariates are acceptable
+  for (extra_covariate_name in extra_covariate_names) {
+    v <- extra_covariates[,extra_covariate_name]
+    accept_type <- is(v, "numeric") || is(v, "character") || is(v, "factor")
+    if (!accept_type) {
+      stop(paste0("The column `", extra_covariate_name, "` of the `extra_covariates` data frame should be of type numeric, character, or factor."))
+    }
+  }
+
+  # 11. verify that moi is specified
   if (!(moi %in% c("low", "high"))) {
     stop("`moi` should be either `low` or `high`.")
   }
