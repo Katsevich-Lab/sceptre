229752c8c9a6a56e06ce3cc7f70813a9b03c0ecc
Tim <tbarry2@andrew.cmu.edu>
2023-08-25 14:26:57 -0400

    fix bug in calculation of n nonzero tot for high moi

--- R/s4_classes_and_functs_p2.R
+++ R/s4_classes_and_functs_p2.R
@@ -1,5 +1,49 @@
-run_calibration_check_v2 <- function(sceptre_object, output_amount = 1, n_calibration_pairs = NULL,
-                                     calibration_group_size = NULL, print_progress = TRUE, parallel = FALSE) {
+run_sceptre_analysis_high_level <- function(sceptre_object, response_grna_group_pairs, calibration_check, output_amount, print_progress, parallel) {
+  # if running permutations, generate the permutation idxs
+  if (sceptre_object@run_permutations) {
+    cat("Generating permutation resamples.")
+    synthetic_idxs <- get_synthetic_permutation_idxs(grna_assignments = sceptre_object@grna_assignments,
+                                                     B = sceptre_object@B1 + sceptre_object@B2 + sceptre_object@B3,
+                                                     calibration_check = calibration_check,
+                                                     control_group_complement = sceptre_object@control_group_complement,
+                                                     calibration_group_size = sceptre_object@calibration_group_size,
+                                                     n_cells = length(sceptre_object@cells_in_use))
+    cat(crayon::green(' \u2713\n'))
+  }
+  gc() |> invisible()
+
+  # initialize the args to pass
+  args_to_pass <- list(response_matrix = sceptre_object@response_matrix,
+                       grna_assignments = sceptre_object@grna_assignments,
+                       covariate_matrix = sceptre_object@covariate_matrix,
+                       response_grna_group_pairs = response_grna_group_pairs,
+                       output_amount = output_amount,
+                       fit_parametric_curve = sceptre_object@fit_parametric_curve,
+                       B1 = sceptre_object@B1, B2 = sceptre_object@B2,
+                       B3 = sceptre_object@B3, calibration_check = calibration_check,
+                       control_group_complement = sceptre_object@control_group_complement,
+                       n_nonzero_trt_thresh = sceptre_object@n_nonzero_trt_thresh,
+                       n_nonzero_cntrl_thresh = sceptre_object@n_nonzero_cntrl_thresh,
+                       side_code = sceptre_object@side_code, low_moi = sceptre_object@low_moi,
+                       response_precomputations = sceptre_object@response_precomputations,
+                       cells_in_use = sceptre_object@cells_in_use,
+                       print_progress = print_progress, parallel = parallel)
+
+  # run the method
+  out <- if (sceptre_object@run_permutations) {
+    args_to_pass$synthetic_idxs <- synthetic_idxs
+    do.call(what = "run_perm_test_in_memory", args = args_to_pass)
+  } else {
+    do.call(what = "run_crt_in_memory_v2", args = args_to_pass)
+  }
+
+  # return the output
+  return(out)
+}
+
+
+run_calibration_check <- function(sceptre_object, output_amount = 1, n_calibration_pairs = NULL,
+                                  calibration_group_size = NULL, print_progress = TRUE, parallel = FALSE) {
   # 0. verify that function called in correct order
   check_function_call(sceptre_object, "run_calibration_check")
 
@@ -12,126 +56,31 @@ run_calibration_check_v2 <- function(sceptre_object, output_amount = 1, n_calibr
 
   # 3. construct the negative control pairs
   cat("Constructing negative control pairs.")
-  response_grna_group_pairs <- construct_negative_control_pairs(n_calibration_pairs = n_calibration_pairs,
-                                                                calibration_group_size = calibration_group_size,
-                                                                grna_assignments = sceptre_object@grna_assignments,
-                                                                response_matrix = sceptre_object@response_matrix,
-                                                                grna_group_data_frame = sceptre_object@grna_group_data_frame,
-                                                                low_moi = sceptre_object@low_moi,
-                                                                n_nonzero_trt_thresh = sceptre_object@n_nonzero_trt_thresh,
-                                                                n_nonzero_cntrl_thresh = sceptre_object@n_nonzero_cntrl_thresh,
-                                                                n_nonzero_m = sceptre_object@M_matrix,
-                                                                n_nonzero_tot = sceptre_object@n_nonzero_tot_vector,
-                                                                cells_in_use = sceptre_object@cells_in_use)
+  response_grna_group_pairs <- construct_negative_control_pairs_v2(sceptre_object = sceptre_object,
+                                                                   n_calibration_pairs = n_calibration_pairs,
+                                                                   calibration_group_size = calibration_group_size)
   cat(crayon::green(' \u2713\n'))
 
+  # 4. update uncached objects
+  sceptre_object@calibration_group_size <- calibration_group_size
+  sceptre_object@n_calibration_pairs <- n_calibration_pairs
+  sceptre_object@last_function_called <- "run_calibration_check"
 
+  # 4. run the sceptre analysis (high-level function call)
+  out <- run_sceptre_analysis_high_level(sceptre_object = sceptre_object,
+                                         response_grna_group_pairs = response_grna_group_pairs,
+                                         calibration_check = TRUE,
+                                         output_amount = output_amount,
+                                         print_progress = print_progress,
+                                         parallel = parallel)
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-run_calibration_check <- function(sceptre_object, output_amount = 1, n_calibration_pairs = NULL,
-                                  calibration_group_size = NULL, print_progress = TRUE, parallel = FALSE) {
-  # 1. do argument check
-  check_calibration_check_inputs(analysis_prepared = sceptre_object@analysis_prepared,
-                                 grna_group_data_frame = sceptre_object@grna_group_data_frame,
-                                 control_group_complement = sceptre_object@control_group_complement) |> invisible()
-
-  # 2. get grna assignments
-  grna_assignments <- sceptre_object@grna_assignments
-
-  # 3. construct the negative control pairs
-  if (is.na(sceptre_object@calibration_group_size)) {
-    calibration_group_size <- compute_calibration_group_size(sceptre_object@grna_group_data_frame)
-  }  else {
-    calibration_group_size <- sceptre_object@calibration_group_size
-  }
-
-  cat("Constructing negative control pairs.")
-  if (nrow(sceptre_object@negative_control_pairs) == 0L) {
-    response_grna_group_pairs <- construct_negative_control_pairs(n_calibration_pairs = sceptre_object@n_calibration_pairs,
-                                                                  calibration_group_size = calibration_group_size,
-                                                                  grna_assignments = grna_assignments,
-                                                                  response_matrix = sceptre_object@response_matrix,
-                                                                  grna_group_data_frame = sceptre_object@grna_group_data_frame,
-                                                                  low_moi = sceptre_object@low_moi,
-                                                                  n_nonzero_trt_thresh = sceptre_object@n_nonzero_trt_thresh,
-                                                                  n_nonzero_cntrl_thresh = sceptre_object@n_nonzero_cntrl_thresh,
-                                                                  n_nonzero_m = sceptre_object@M_matrix,
-                                                                  n_nonzero_tot = sceptre_object@n_nonzero_tot_vector)
-  } else {
-    response_grna_group_pairs <- sceptre_object@negative_control_pairs
-  }
-  cat(crayon::green(' \u2713\n'))
-
-  # 4. generate the set of synthetic indicator idxs (if running permutations)
-  if (sceptre_object@run_permutations) {
-    cat("Generating permutation resamples.")
-    n_cells <- nrow(sceptre_object@covariate_matrix)
-    synthetic_idxs <- get_synthetic_permutation_idxs(grna_assignments = grna_assignments,
-                                                     B = sceptre_object@B1 + sceptre_object@B2 + sceptre_object@B3,
-                                                     calibration_check = TRUE,
-                                                     control_group_complement = sceptre_object@control_group_complement,
-                                                     calibration_group_size = calibration_group_size,
-                                                     n_cells = n_cells)
-    cat(crayon::green(' \u2713\n'))
-  }
-  gc() |> invisible()
-
-  # 5. run the method
-  if (sceptre_object@run_permutations) {
-    out <- run_perm_test_in_memory(response_matrix = sceptre_object@response_matrix,
-                                   grna_assignments = grna_assignments,
-                                   covariate_matrix = sceptre_object@covariate_matrix,
-                                   response_grna_group_pairs = response_grna_group_pairs,
-                                   synthetic_idxs = synthetic_idxs,
-                                   output_amount = output_amount,
-                                   fit_parametric_curve = sceptre_object@fit_parametric_curve,
-                                   B1 = sceptre_object@B1, B2 = sceptre_object@B2,
-                                   B3 = sceptre_object@B3, calibration_check = TRUE,
-                                   control_group_complement = sceptre_object@control_group_complement,
-                                   n_nonzero_trt_thresh = sceptre_object@n_nonzero_trt_thresh,
-                                   n_nonzero_cntrl_thresh = sceptre_object@n_nonzero_cntrl_thresh,
-                                   side_code = sceptre_object@side_code, low_moi = sceptre_object@low_moi,
-                                   response_precomputations = sceptre_object@response_precomputations,
-                                   print_progress = print_progress, parallel = parallel)
-  } else {
-    stop("CRT not yet implemented.")
-    ret <- run_crt_in_memory_v2(response_matrix = sceptre_object@response_matrix,
-                                grna_assignments = grna_assignments,
-                                covariate_matrix = sceptre_object@covariate_matrix,
-                                response_grna_group_pairs = response_grna_group_pairs,
-                                output_amount = output_amount,
-                                fit_parametric_curve = sceptre_object@fit_parametric_curve,
-                                B1 = sceptre_object@B1, B2 = sceptre_object@B2,
-                                B3 = sceptre_object@B3, calibration_check = TRUE,
-                                control_group_complement = sceptre_object@control_group_complement,
-                                n_nonzero_trt_thresh = sceptre_object@n_nonzero_trt_thresh,
-                                n_nonzero_cntrl_thresh = sceptre_object@n_nonzero_cntrl_thresh,
-                                side_code = sceptre_object@side_code, low_moi = sceptre_object@low_moi,
-                                print_progress = print_progress)
-  }
-
-  # update fields of sceptre object
-  sceptre_object@calibration_result <- out$ret_pass_qc
+  # update fields of sceptre object with results
+  sceptre_object@calibration_result <- out$result
   sceptre_object@negative_control_pairs <- response_grna_group_pairs
   sceptre_object@response_precomputations <- out$response_precomputations
-  sceptre_object@calibration_check_run <- TRUE
-  sceptre_object@last_function_called <- "run_calibration_check"
   return(sceptre_object)
 }
+
 
 
 run_power_check <- function(sceptre_object, output_amount = 1, print_progress = TRUE, parallel = FALSE) {
