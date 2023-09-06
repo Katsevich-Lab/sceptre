242d347d7d4d405ef35a23533a6163509b6818a5
Tim <tbarry2@andrew.cmu.edu>
2023-09-06 17:49:54 -0400

    update parallel logging for assign grnas

--- R/mixture_model_functs.R
+++ R/mixture_model_functs.R
@@ -1,4 +1,4 @@
-assign_grnas_to_cells_mixture <- function(grna_matrix, cell_covariate_data_frame, grna_assignment_hyperparameters, parallel) {
+assign_grnas_to_cells_mixture <- function(grna_matrix, cell_covariate_data_frame, grna_assignment_hyperparameters, print_progress, parallel) {
   if (!parallel) cat(crayon::red("Note: Set `parallel = TRUE` in the function call to improve speed.\n\n"))
   # 0. get random starting guesses for pi and g_pert
   starting_guesses <- get_random_starting_guesses(n_em_rep = grna_assignment_hyperparameters$n_em_rep,
@@ -6,10 +6,8 @@ assign_grnas_to_cells_mixture <- function(grna_matrix, cell_covariate_data_frame
                                                   g_pert_guess_range = grna_assignment_hyperparameters$g_pert_guess_range)
 
   # 1. obtain the covariate matrix
-  formula_object <- auto_construct_formula_object(cell_covariates = cell_covariate_data_frame,
-                                                  include_grna_covariates = TRUE)
   covariate_matrix <- convert_covariate_df_to_design_matrix(covariate_data_frame = cell_covariate_data_frame,
-                                                            formula_object = formula_object)
+                                                            formula_object = grna_assignment_hyperparameters$formula_object)
 
   # 2. make the grna expression matrix row-accessible
   grna_matrix <- set_matrix_accessibility(grna_matrix, make_row_accessible = TRUE)
@@ -17,16 +15,15 @@ assign_grnas_to_cells_mixture <- function(grna_matrix, cell_covariate_data_frame
 
   # 3. define the function to perform assignments for a set of grnas
   analyze_given_grna_ids <- function(curr_grna_ids, proc_id = NULL) {
-    if (parallel) {
+    if (parallel && print_progress) {
       f_name <- paste0(get_log_dir(), "assign_grnas_", proc_id, ".out")
       file.create(f_name) |> invisible()
+    } else {
+      f_name <- NULL
     }
     initial_assignment_list <- sapply(seq_along(curr_grna_ids), function(i) {
-      grna_id <- curr_grna_ids[i]
-      if (i == 1 || i %% 5 == 0) {
-        msg <- paste0("Carrying out gRNA-to-cell assignments for gRNA ", grna_id, " (", i, " of ", length(curr_grna_ids), ")\n")
-        if (parallel) write(x = msg, file = f_name, append = TRUE) else cat(msg)
-      }
+      grna_id <- get_id_from_idx(response_idx = i, print_progress = print_progress, response_ids = curr_grna_ids,
+                                 feature = "gRNA", str = "Performing gRNA-to-cell assignments for", parallel = parallel, f_name = f_name)
       g <- load_csr_row(j = grna_matrix@j, p = grna_matrix@p, x = grna_matrix@x,
                         row_idx = which(grna_id == grna_ids), n_cells = ncol(grna_matrix))
       assignments <- obtain_em_assignments(pi_guesses = starting_guesses$pi_guesses,
@@ -43,15 +40,17 @@ assign_grnas_to_cells_mixture <- function(grna_matrix, cell_covariate_data_frame
   if (!parallel) {
     initial_assignment_list <- analyze_given_grna_ids(grna_ids)
   } else {
-    cat(paste0("Running gRNA assignments in parallel. Change directories to ", crayon::blue(get_log_dir()),
-               " and view the files ", crayon::blue("assign_grnas_*.out"), " for progress updates.\n"))
+    cat("Running gRNA assignments in parallel. ")
+    if (print_progress) {
+      cat(paste0("Change directories to ", crayon::blue(get_log_dir()), " and view the files ",
+                 crayon::blue("assign_grnas_*.out"), " for progress updates.\n"))
+    }
     grna_ids_partitioned <- partition_response_ids(grna_ids, parallel)
     res <- parallel::mclapply(seq_along(grna_ids_partitioned),
                               function(proc_id) analyze_given_grna_ids(grna_ids_partitioned[[proc_id]], proc_id),
                               mc.cores = length(grna_ids_partitioned))
     initial_assignment_list <- res |> purrr::flatten()
   }
-
 
   # return
   return (initial_assignment_list)
