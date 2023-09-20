cc4c86eeee87a6389603f5d9e3a476e499fa0586
Tim <tbarry2@andrew.cmu.edu>
2023-09-20 12:36:43 -0400

    remove all factoring of grna id, grna target, grna group, and response id (for now)

--- data-raw/DATASET_highmoi.R
+++ data-raw/DATASET_highmoi.R
@@ -130,7 +130,7 @@ extra_covariates_highmoi <- covariate_data_frame_highmoi |>
   dplyr::mutate(batch = factor(batch, levels = c("prep_batch_1", "prep_batch_2"),
                                labels = c("b1", "b2")))
 gene_names_highmoi <- gene_table$gene_name[match(rownames(response_matrix_highmoi), gene_table$gene_id)]
-grna_target_data_frame_highmoi <- grna_group_data_frame_highmoi |> dplyr::rename("grna_target" = "grna_group")
+grna_target_data_frame_highmoi <- grna_group_data_frame_highmoi |> dplyr::rename("grna_target" = "grna_group") |> as.data.frame()
 
 # save
 usethis::use_data(response_matrix_highmoi, gene_names_highmoi, grna_matrix_highmoi,
