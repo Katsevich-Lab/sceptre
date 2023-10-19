d6d229ed830287eafe6374a0f2644e7d311ddfbb
Tim <tbarry2@andrew.cmu.edu>
2023-10-19 12:47:54 -0400

    pass check

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
-  cat(paste0(crayon::blue("Welcome to sceptre.\nSubmit issues on the sceptre website: "),
-             crayon::red("github.com/Katsevich-Lab/sceptre"),
-             crayon::blue("\nRead the sceptre manual: "),
-             crayon::red("timothy-barry.github.io/sceptre-book/")))
+  packageStartupMessage(paste0(crayon::blue("Welcome to sceptre.\nSubmit issues on the sceptre website: "),
+                               crayon::red("github.com/Katsevich-Lab/sceptre"),
+                               crayon::blue("\nRead the sceptre manual: "),
+                               crayon::red("timothy-barry.github.io/sceptre-book/")))
 }
