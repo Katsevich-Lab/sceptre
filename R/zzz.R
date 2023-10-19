2841f260f086c88ccad50c8647b32d1bf90069b3
Tim <tbarry2@andrew.cmu.edu>
2023-10-19 13:02:42 -0400

    fix package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
 .onLoad <- function(libname, pkgname) {
-  packageStartupMessage(paste0(crayon::blue("Welcome to sceptre.\nSubmit issues on the sceptre website: "),
-                               crayon::red("github.com/Katsevich-Lab/sceptre"),
-                               crayon::blue("\nRead the sceptre manual: "),
-                               crayon::red("timothy-barry.github.io/sceptre-book/")))
+  #packageStartupMessage(paste0(crayon::blue("Welcome to sceptre.\nSubmit issues on the sceptre website: "),
+  #                             crayon::red("github.com/Katsevich-Lab/sceptre"),
+  #                             crayon::blue("\nRead the sceptre manual: "),
+  #                             crayon::red("timothy-barry.github.io/sceptre-book/")))
+
+  packageStartupMessage("Welcome to sceptre.\nSubmit issues on the sceptre website: github.com/Katsevich-Lab/sceptre\nRead the sceptre manual: timothy-barry.github.io/sceptre-book/")
 }
