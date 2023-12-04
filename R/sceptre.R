cfa7b7de54f5d9f2811c43308e5364f13d144e29
Tim Barry <tbarry2@andrew.cmu.edu>
2023-12-04 14:09:17 -0500

    finish draft of allowing odm-backed sceptre_objects within package

--- R/sceptre.R
+++ R/sceptre.R
@@ -128,55 +128,18 @@ utils::globalVariables(c("n_nonzero_trt", "n_nonzero_cntrl", "pair_str", "assign
 #' #####################
 #' # out-of-core example
 #' #####################
-#' # 0. set file paths
-#' base_dir <- "/Users/timbarry/research_offsite/external/replogle-2022/raw/rd7/rpe1_other"
-#' directories <- list.files(base_dir, full.names = TRUE)[1:3]
-#' directory_to_write <- "/Users/timbarry/research_offsite/external/replogle-2022/processed/rd7/small"
-#' moi <- "low"
-#' grna_target_data_frame <- readRDS("/Users/timbarry/research_offsite/external/replogle-2022/raw/rd7/grna_target_data_frame.rds")
+#' # 1. create the sceptre object, setting `ondisc = TRUE`
+#' # and specifying the `directory_to_write` in which to write
+#' # the backing `.odm` files.
+#' directories <- paste0(system.file("extdata", package = "sceptre"),
+#' "/highmoi_example/gem_group_", 1:2)
+#' data(grna_target_data_frame_highmoi)
+#' sceptre_object <- import_data_from_cellranger(directories = directories,
+#' moi = "high",
+#' grna_target_data_frame = grna_target_data_frame_highmoi,
+#' use_ondisc = TRUE,
+#' directory_to_write = tempdir())
 #'
-#' # 1. import data
-#' sceptre_object <- import_data_from_cellranger(
-#'    directories = directories,
-#'    directory_to_write = directory_to_write,
-#'    moi = moi,
-#'    grna_target_data_frame = grna_target_data_frame,
-#'    use_ondisc = TRUE)
-#'
-#' # 2. set analysis parameters
-#' discovery_pairs <- construct_trans_pairs(
-#'   sceptre_object = sceptre_object) |> dplyr::sample_n(1000000)
-#' sceptre_object <- set_analysis_parameters(
-#'   sceptre_object = sceptre_object,
-#'   discovery_pairs = discovery_pairs)
-#'
-#' # 3. set analysis parameters
-#' sceptre_object <- sceptre_object |> set_analysis_parameters(
-#'   discovery_pairs = discovery_pairs
-#' )
-#'
-#' # 4. write sceptre_object to disk
-#' write_ondisc_backed_sceptre_object(
-#'   sceptre_object,
-#'   paste0(directory_to_write, "/sceptre_object.rds")
-#' )
-#'
-#' # 5. read sceptre_object from disk
-#' sceptre_object <- read_ondisc_backed_sceptre_object(
-#'   paste0(directory_to_write, "/sceptre_object.rds"),
-#'   paste0(directory_to_write, "/gene.odm"),
-#'   paste0(directory_to_write, "/grna.odm")
-#' )
-#'
-#' # 6. assign gRNAs
-#' sceptre_object <- sceptre_object |>
-#'   assign_grnas(
-#'     method = "mixture",
-#'     parallel = TRUE
-#'   )
-#'
-#' # 7. run qc
-#' sceptre_object <- run_qc(sceptre_object)
-#'
+#' # 2. repeat steps 2-8 above.
 #' }
 NULL
