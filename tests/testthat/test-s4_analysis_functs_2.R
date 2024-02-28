726db1994062388ced36eaa79ba3ccd49d5331d2
jdeu1023 <jdeu@wharton.upenn.edu>
2024-02-28 12:11:46 -0500

    add test for run_discovery_analysis()

--- tests/testthat/test-s4_analysis_functs_2.R
+++ tests/testthat/test-s4_analysis_functs_2.R
@@ -324,3 +324,80 @@ test_that("run_power_check", {
   # log fold change should be 0 here because the response_matrix values are constant
   expect_true(dplyr::filter(scep_power@power_result, response_id == "t3") |> dplyr::pull(log_2_fold_change) |> is.na())
 })
+
+
+
+test_that("run_discovery_analysis", {
+  grna_target_data_frame <- data.frame(
+    grna_id = c("id1", "id2", "id3", "nt1", "nt2"),
+    grna_target = c("t1", "t2", "t3", "non-targeting", "non-targeting"),
+    chr = "", start = 0, end = 1
+  )
+  num_grna <- nrow(grna_target_data_frame)
+  num_cells <- 50
+  num_responses <- 10
+
+  set.seed(1)
+  # using sample(0:1) so no entries can accidentally cross the threshold
+  grna_matrix <- matrix(sample(0:1, num_grna * num_cells, replace=TRUE), num_grna, num_cells) |>
+    `rownames<-`(grna_target_data_frame$grna_id)
+  cells_expressing_t1 <- 1:10
+  cells_expressing_t2 <- 11:20
+  cells_expressing_t3 <- 21:30
+  cells_expressing_nt1 <- 31:40
+  cells_expressing_nt2 <- 41:50
+  all_cells <- 1:num_cells
+
+  grna_matrix["id1", cells_expressing_t1] <- 50
+  grna_matrix["id2", cells_expressing_t2] <- 50
+  # grna_matrix["id3", cells_expressing_t3] <- 50
+  grna_matrix["nt1", cells_expressing_nt1] <- 50
+  grna_matrix["nt2", cells_expressing_nt2] <- 50
+
+  response_matrix <- matrix(rpois( num_responses * num_cells, 1), num_responses, num_cells) |>
+    `rownames<-`(c("t1", "t2", "t3", paste0("response_", 4:num_responses)))
+
+  response_matrix["t1", cells_expressing_t1] <- 100 # should be highly significant
+
+  response_matrix["t2", ] <- 100 # should not be significant at all
+
+  positive_control_pairs = data.frame(
+    grna_target = c("t1", "t2", "t3"),
+    response_id = c("t1", "t2", "t3")
+  )
+  discovery_pairs <- data.frame(
+    grna_target = c("t1",        "t1",        "t2",         "t2"),
+    response_id = c("response_4", "response_5",  "response_4", "response_6")
+  )
+
+  ## testing `control_group = "nt_cells"` ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  disc_results <- import_data(
+    grna_matrix = grna_matrix,
+    response_matrix = response_matrix,
+    grna_target_data_frame = grna_target_data_frame,
+    moi = "low"
+  ) |>
+    set_analysis_parameters(
+      positive_control_pairs = positive_control_pairs,
+      discovery_pairs = discovery_pairs,
+      control_group = "nt_cells"
+    ) |>
+    assign_grnas(method = "thresholding", threshold = 40) |>
+    # don't want to remove any cells for this one
+    run_qc(response_n_umis_range = c(0, 1), response_n_nonzero_range = c(0,1),
+           # making the response_5, t1 pair fail pairwise QC
+           n_nonzero_trt_thresh = 7, n_nonzero_cntrl_thresh = 0) |>
+    run_calibration_check(calibration_group_size = 1) |>
+    run_power_check() |>
+    run_discovery_analysis() |>
+    get_result(analysis = "run_discovery_analysis")
+
+  # confirming everything is ok with the pair failing pairwise QC
+  expect_false(disc_results[grna_target == "t1" & response_id == "response_5", "pass_qc"][[1]])
+
+  # t1 should be significant
+  expect_true(disc_results[grna_target == "t1" & response_id == "response_4","significant"][[1]])
+
+  # t2 tests are not significant
+  expect_false(any(disc_results[grna_target == "t2","significant"]))
+})
