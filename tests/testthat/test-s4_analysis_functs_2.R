97ded4bf9b9dd1b17082484c364734cae9deb215
jdeu1023 <jdeu@wharton.upenn.edu>
2024-02-28 11:46:22 -0500

    finish neg cntrl pair tests

--- tests/testthat/test-s4_analysis_functs_2.R
+++ tests/testthat/test-s4_analysis_functs_2.R
@@ -110,10 +110,6 @@ test_that("run_calibration_check negative control pairs complement set with cell
            n_nonzero_trt_thresh = 0, n_nonzero_cntrl_thresh = 20) |>
     run_calibration_check(calibration_group_size=1)
 
-  scep_complement_size_1@negative_control_pairs
-  scep_complement_size_1@discovery_pairs_with_info
-
-
   # making sure the correct cells were removed
   remaining_cells <- all_cells[-c(cells_to_remove_low_umi, cells_to_remove_high_umi)]
   expect_setequal(scep_complement_size_1@cells_in_use, remaining_cells)
@@ -144,8 +140,7 @@ test_that("run_calibration_check negative control pairs complement set with cell
   set.seed(2)
   scep_complement_size_2 <- scep_pre |>
     run_qc(response_n_umis_range = c(0, .90), response_n_nonzero_range = c(.15, 1),
-           # with `n_nonzero_trt_thresh = 1` every neg ctrl pair with this seed passes,
-           # but we lose one because an actual discovery pair now fails QC
+           # with `n_nonzero_trt_thresh = 1` a single discovery pair fails pairwise QC
            n_nonzero_trt_thresh = 1, n_nonzero_cntrl_thresh = 0) |>
     run_calibration_check(calibration_group_size=2)
 
@@ -177,7 +172,89 @@ test_that("run_calibration_check negative control pairs complement set with cell
 })
 
 test_that("run_calibration_check negative control pairs nt set with cellwise qc", {
+  grna_target_data_frame <- data.frame(
+    grna_id = c("id1", "id2", "id3", "nt1", "nt2"),
+    grna_target = c("t1", "t2", "t3", "non-targeting", "non-targeting"),
+    chr = "", start = 0, end = 1
+  )
+  num_grna <- nrow(grna_target_data_frame)
+  num_cells <- 50
+  num_responses <- 10
 
+  set.seed(1)
+  # using sample(0:1) so no entries can accidentally cross the threshold
+  grna_matrix <- matrix(sample(0:1, num_grna * num_cells, replace=TRUE), num_grna, num_cells) |>
+    `rownames<-`(grna_target_data_frame$grna_id)
+  cells_expressing_t1 <- 1:10
+  cells_expressing_t2 <- 11:20
+  cells_expressing_t3 <- 21:30
+  cells_expressing_nt1 <- 31:40
+  cells_expressing_nt2 <- 41:50
+  all_cells <- 1:num_cells
+
+  grna_matrix["id1", cells_expressing_t1] <- 50
+  grna_matrix["id2", cells_expressing_t2] <- 50
+  # grna_matrix["id3", cells_expressing_t3] <- 50
+  grna_matrix["nt1", cells_expressing_nt1] <- 50
+  grna_matrix["nt2", cells_expressing_nt2] <- 50
+
+  response_matrix <- matrix(rpois( num_responses * num_cells, 1), num_responses, num_cells) |>
+    `rownames<-`(c("t1", "t2", "t3", paste0("response_", 4:num_responses)))
+
+  cells_to_remove_low_umi <- c(1,2,4,5,6,11,12,31)
+  cells_to_remove_high_umi <- c(3, 13,32,33,34)
+  response_matrix[,cells_to_remove_low_umi] <- 0
+  response_matrix[,cells_to_remove_high_umi] <- 100000
+
+  discovery_pairs <- data.frame(
+    grna_target = c("t1",         "t2",         "t3"),
+    response_id = c("response_4", "response_5", "response_6")
+  )
+
+  ## testing `control_group = "complement"` and `calibration_group_size=1` ~~~~~~~~~~~~~~~~~~~~~
+  scep_pre <- import_data(
+    grna_matrix = grna_matrix,
+    response_matrix = response_matrix,
+    grna_target_data_frame = grna_target_data_frame,
+    moi = "low"
+  ) |>
+    set_analysis_parameters(
+      discovery_pairs = discovery_pairs,
+      control_group = "nt_cells"
+    ) |>
+    assign_grnas(method = "thresholding", threshold = 40)
+
+  scep_nt_size_1 <- scep_pre |>
+    run_qc(response_n_umis_range = c(0, .90), response_n_nonzero_range = c(.15, 1),
+           # with `n_nonzero_cntrl_thresh = 20` one discovery pair fails
+           n_nonzero_trt_thresh = 1, n_nonzero_cntrl_thresh = 0) |>
+    run_calibration_check(calibration_group_size=1)
+
+  # making sure the correct cells were removed
+  remaining_cells <- all_cells[-c(cells_to_remove_low_umi, cells_to_remove_high_umi)]
+  expect_setequal(scep_nt_size_1@cells_in_use, remaining_cells)
+
+  neg_df <- scep_nt_size_1@negative_control_pairs
+
+  # a single neg ctrl pair with this seed fails QC so there are 2 left
+  expect_equal(nrow(neg_df), sum(scep_nt_size_1@discovery_pairs_with_info$pass_qc))
+  expect_true(all(neg_df$pass_qc))
+
+  for(i in 1:nrow(neg_df)) {
+    trt_cells <- remaining_cells[scep_nt_size_1@grna_assignments$all_nt_idxs[scep_nt_size_1@grna_assignments$indiv_nt_grna_idxs[[neg_df$grna_group[i]]]]]
+    expect_equal(
+      neg_df$n_nonzero_trt[i],
+      sum(response_matrix[neg_df$response_id[i], trt_cells] > 0)
+    )
+    # nt control group
+    other_nt <- setdiff(c("nt1", "nt2"), neg_df$grna_group[i])
+
+    cntrl_cells <- remaining_cells[scep_nt_size_1@grna_assignments$all_nt_idxs[scep_nt_size_1@grna_assignments$indiv_nt_grna_idxs[[other_nt]]]]
+    expect_equal(
+      neg_df$n_nonzero_cntrl[i],
+      sum(response_matrix[neg_df$response_id[i], cntrl_cells] > 0)
+    )
+  }
 })
 
 
