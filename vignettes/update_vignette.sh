a65f6adc8d334e5ec4d6dd2231c38e24630eb9d3
timothy-barry <tbarry2@andrew.cmu.edu>
2024-03-05 19:35:10 -0500

    begin package cleanup

--- vignettes/update_vignette.sh
+++
@@ -1,6 +0,0 @@
-cp ~/research_code/sceptre-book/sceptre.qmd ~/research_code/sceptre/vignettes/sceptre.Rmd
-
-sed -i -e '/::: callout-note/,/:::/d' sceptre.Rmd
-sed -i -e 's/# The whole game {#sec-whole_game .unnumbered}//g' sceptre.Rmd
-
-rm sceptre.Rmd-e
