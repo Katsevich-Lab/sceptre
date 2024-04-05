7380dcbc5f455ff61f7f0d8b87ceebfb21b58b7c
ekatsevi <katsevich.gene@gmail.com>
2024-04-05 14:34:57 -0400

    Fixed bug for resampling_approximation = standard_normal

--- R/s4_helpers.R
+++ R/s4_helpers.R
@@ -98,7 +98,7 @@ setMethod("print", signature = signature("sceptre_object"), function(x) {
     } else {
       paste0("\n\t\U2022 Control group: ", if (length(x@control_group_complement) == 0L) "not specified" else crayon::blue(ifelse(x@control_group_complement, "complement set", "non-targeting cells")))
     },
-    "\n\t\U2022 Resampling mechanism: ", if (length(x@run_permutations) == 0L) "not specified" else crayon::blue(ifelse(x@run_permutations, "permutations", "conditional resampling")),
+    "\n\t\U2022 Resampling mechanism: ", if (length(x@run_permutations) == 0L) "not specified" else if(x@resampling_approximation == "standard_normal") crayon::blue("no resampling") else crayon::blue(ifelse(x@run_permutations, "permutations", "conditional resampling")),
     "\n\t\U2022 gRNA integration strategy: ", if (length(x@grna_integration_strategy) == 0L) "not specified" else crayon::blue(x@grna_integration_strategy),
     "\n\t\U2022 Resampling approximation: ", if (length(x@resampling_approximation) == 0L) "not specified" else crayon::blue(gsub(pattern = "_", replacement = " ", fixed = TRUE, x = x@resampling_approximation)),
     "\n\t\U2022 Multiple testing adjustment: ", if (x@nuclear || length(x@multiple_testing_method) == 0L) "none" else paste0(crayon::blue(x@multiple_testing_method), " at level ", crayon::blue(x@multiple_testing_alpha)),
