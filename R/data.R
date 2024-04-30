84142ba0e14b2d2df35cf2f70c83c6ed3436514b
timothy-barry <tbarry2@andrew.cmu.edu>
2024-04-29 22:17:32 -0400

    finish updates of grch38 and grch37 data frams

--- R/data.R
+++ R/data.R
@@ -1,8 +1,13 @@
-#' Gene position data frame
+#' Gene position data frames
 #'
-#' `gene_position_data_frame_grch38` maps each gene to the chromosome on which it is located and the position of its transcription start site on that chromosome. The data frame was constructed from the GRCh38 reference genome that has shipped with CellRanger since 2020.
+#' `gene_position_data_frame_grch38` and `gene_position_data_frame_grch37` contain the coordinate and transcription start site position of each gene relative to reference genome GRCh38 and GRCh37, respectively. Both `gene_position_data_frame_grch38` and `gene_position_data_frame_grch37` were constructed from reference genomes available on the 10x Genomics website. The GRCh38 reference genome has been used by 10x Cell Ranger since 2020.
 #'
 #' @usage data(gene_position_data_frame_grch38)
 #' @examples
 #' head(gene_position_data_frame_grch38)
+#' head(gene_position_data_frame_grch37)
 "gene_position_data_frame_grch38"
+
+#' @rdname gene_position_data_frame_grch38
+#' @usage data(gene_position_data_frame_grch37)
+"gene_position_data_frame_grch37"
