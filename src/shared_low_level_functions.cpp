2923a67cb3bc4ba7c1ee1356ddce177964321349
timothy-barry <tbarry@hsph.harvard.edu>
2025-04-03 19:20:45 -0400

    add draft ci functionality

--- src/shared_low_level_functions.cpp
+++ src/shared_low_level_functions.cpp
@@ -17,7 +17,7 @@ std::vector<double> estimate_log_fold_change_v2(NumericVector y, NumericVector m
     sum_mu += mu[trt_idxs[i] - 1];
     sum_y2 += curr_y * curr_y;
   }
-  double fc = sum_y/n_trt_double - sum_mu/n_trt_double;
+  double fc = (sum_y/n_trt_double)/(sum_mu/n_trt_double);
   double se_over_root_n = sqrt((n_trt_double * sum_y2 - sum_y * sum_y)/(n_trt_double * sum_mu * sum_mu));
   return std::vector<double> {fc, se_over_root_n};
 }
