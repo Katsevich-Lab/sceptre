9f5088709801c599dc20a769cac7f2544e9fd03f
Eugene Katsevich <katsevich.gene@gmail.com>
2026-06-02 23:07:35 -0400

    Drop deprecated fisher_yates typo alias

--- R/deprecated.R
+++
@@ -1,8 +0,0 @@
-# Deprecated function aliases kept for backward compatibility.
-
-#' @keywords internal
-#' @noRd
-fisher_yates_samlper <- function(n_tot, M, B) {
-  .Deprecated("fisher_yates_sampler")
-  fisher_yates_sampler(n_tot = n_tot, M = M, B = B)
-}
